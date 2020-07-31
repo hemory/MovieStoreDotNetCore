6d6b18f45bf4896352c3b10ab849f607e81dc063
Hemory Phifer <hemphife@gmail.com>
2020-07-31 09:36:12 -0400

    added the controller and corrected the JSon service name

---
+++ MovieStore/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Models;
+using MovieStore.Services;
+using System.Collections.Generic;
+
+namespace MovieStore.Controllers
+{
+    public class MovieController : ControllerBase
+    {
+        public JSonFileMoviesService MovieService { get; }
+        public MovieController(JSonFileMoviesService movieService)
+        {
+            MovieService = movieService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return MovieService.GetMovies();
+        }
+    }
+}
