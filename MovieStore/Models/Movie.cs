5e92a7a0534c8c4bbf083edad5530ec977051883
Hemory Phifer <hemphife@gmail.com>
2020-07-31 09:21:37 -0400

    added the JSonFileProductService Class

--- MovieStore/Models/Movie.cs
+++ MovieStore/Models/Movie.cs
@@ -2,9 +2,10 @@
 {
     public class Movie
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Image { get; set; }
+        public int[] Ratings { get; set; }
     }
 }
---
+++ MovieStore/Services/JSonFileProductsService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using MovieStore.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace MovieStore.Services
+{
+    public class JSonFileProductsService
+    {
+        public JSonFileProductsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+
+        //This gets the json file at the root of the directory
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "movies.json"); }
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Movie[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string movieId, int rating)
+        {
+            var movies = GetMovies();
+
+            if (movies.First(x => x.Id == movieId).Ratings == null)
+            {
+                movies.First(x => x.Id == movieId).Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = movies.First(x => x.Id == movieId).Ratings.ToList();
+                ratings.Add(rating);
+                movies.First(x => x.Id == movieId).Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Movie>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    movies
+                );
+            }
+        }
+    }
+}
