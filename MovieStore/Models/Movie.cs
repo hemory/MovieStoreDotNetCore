a50279901b984071eee881580ae86026feaf44c7
Hemory Phifer <hemphife@gmail.com>
2020-07-31 09:14:37 -0400

    added model to align with JSon data

---
+++ MovieStore/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieStore.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Image { get; set; }
+    }
+}
